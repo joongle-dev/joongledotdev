2a9c0623e87d50dd17d54ed4ff327da8f1cc9aeb
Joongho <leejoongho42@gmail.com>
2023-08-18 13:08:41 -0400

    yahtzee client setup

---
+++ yahtzee/src/graphics/mod.rs
@@ -0,0 +1,114 @@
+use winit::{window::Window, platform::web::WindowExtWebSys};
+
+pub struct Renderer {
+    surface: wgpu::Surface,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    config: wgpu::SurfaceConfiguration,
+    size: winit::dpi::PhysicalSize<u32>,
+    window: Window,
+}
+
+impl Renderer {
+    pub async fn new(window: Window) -> Self {
+        let size = window.inner_size();
+
+        let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
+            backends: wgpu::Backends::all(),
+            dx12_shader_compiler: Default::default(),
+        });
+
+        let surface = match instance.create_surface_from_canvas(window.canvas()) {
+            Ok(surface) => surface,
+            Err(error) => panic!("Failed to create surface from canvas: {error}")
+        };
+
+        let adapter = match instance.request_adapter(
+            &wgpu::RequestAdapterOptions {
+                power_preference: wgpu::PowerPreference::default(),
+                compatible_surface: Some(&surface),
+                force_fallback_adapter: true,
+            }
+        ).await {
+            Some(adapter) => adapter,
+            None => panic!("Faild graphics adapter request.")
+        };
+        let adapter_info = adapter.get_info();
+        log::info!("Graphics adapter name: {}", adapter_info.name);
+
+        let (device, queue) = match adapter.request_device(
+            &wgpu::DeviceDescriptor {
+                features: wgpu::Features::empty(),
+                limits: wgpu::Limits::default(), //TODO: Consider WebGL compatibility.
+                label: Some("WGPU Device"),
+            }, 
+            None
+        ).await {
+            Ok(device_queue) => device_queue,
+            Err(error) => panic!("Failed graphics device request: {error}")
+        };
+
+        let surface_capabilities = surface.get_capabilities(&adapter);
+        let surface_format = surface_capabilities.formats.iter()
+            .copied()
+            .find(|format| format.is_srgb())
+            .unwrap_or(surface_capabilities.formats[0]);
+        let config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format: surface_format,
+            width: size.width,
+            height: size.height,
+            present_mode: surface_capabilities.present_modes[0], //TODO: Present mode setting.
+            alpha_mode: surface_capabilities.alpha_modes[0], 
+            view_formats: vec![]
+        };
+        surface.configure(&device, &config);
+
+        Self {
+            surface,
+            device,
+            queue,
+            config,
+            size,
+            window
+        }
+    }
+
+    pub fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
+        if new_size.width > 0 && new_size.height > 0 {
+            self.size = new_size;
+            self.config.width = new_size.width;
+            self.config.height = new_size.height;
+            self.surface.configure(&self.device, &self.config);
+        }
+    }
+
+    pub fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
+        let output = self.surface.get_current_texture()?;
+        let view = output.texture.create_view(&wgpu::TextureViewDescriptor::default());
+        let mut encoder = self.device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
+            label: Some("Render Encoder"),
+        });
+        
+        {
+            let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                label: Some("Render Pass"),
+                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                    view: &view,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(wgpu::Color {
+                            r: 0.1,
+                            g: 0.2,
+                            b: 0.3,
+                            a: 1.0,
+                        }),
+                        store: true,
+                    },
+                })],
+                depth_stencil_attachment: None,
+            });
+        }
+        Ok(())
+    }
+}
--- yahtzee/src/lib.rs
+++ yahtzee/src/lib.rs
@@ -1,14 +1,42 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+use wasm_bindgen::prelude::*;
+use winit::{
+    event::*,
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+    platform::web::WindowBuilderExtWebSys,
+};
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+mod graphics;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[wasm_bindgen]
+pub async fn run(canvas: web_sys::HtmlCanvasElement) {
+    std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+    console_log::init_with_level(log::Level::Info)
+        .expect("Failed to initialize logger!");
+
+    let event_loop = EventLoop::new();
+    let window = match WindowBuilder::new().with_canvas(Some(canvas)).build(&event_loop) {
+        Ok(window) => window,
+        Err(error) => panic!("Failed to create winit window from canvas: {error}")
+    };
+    let renderer = graphics::Renderer::new(window).await;
+
+    event_loop.run(move |event, _, control_flow| { 
+        match event {
+            Event::WindowEvent { window_id, ref event } if window_id == window.id() => {
+                match event {
+                    WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+                    WindowEvent::Resized(new_size) => renderer.resize(new_size),
+                    _ => {}
+                }
+            } 
+            Event::RedrawRequested(window_id) if window_id == window.id() => {
+                
+            }
+            Event::MainEventsCleared => {
+                window.request_redraw();
+            }
+            _ => {}
+        }
+    });
 }
