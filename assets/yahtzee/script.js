7363232fc2528b8f0d2e436cf53f03e586e1283e
Joongho <leejoongho42@gmail.com>
2023-08-24 03:28:29 -0400

    remove test code

--- assets/yahtzee/script.js
+++ assets/yahtzee/script.js
@@ -1,17 +1,5 @@
 import init, { run } from './yahtzee.js';
 
-const socket_address = 'wss://' + window.location.host + '/yahtzee/ws';
-console.log('connecting to ' +  socket_address);
-const socket = new WebSocket(socket_address);
-
-socket.addEventListener('open', function(event) {
-    console.log('websocket connection established');
-});
-
-socket.addEventListener('message', function(event) {
-    console.log('websocket message received');
-});
-
 const canvas = document.getElementById('canvas');
 async function run_yahtzee() {
     await init();
