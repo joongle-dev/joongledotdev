033f7a6451b43cb04cae2468243492ea1eadd05f
Joongho <leejoongho42@gmail.com>
2023-08-31 08:44:27 -0400

    refactoring

--- crates-wasm32/yahtzee/src/networks/network.rs
+++ crates-wasm32/yahtzee/src/networks/network.rs
@@ -1,21 +1,55 @@
-use wasm_bindgen::closure::Closure;
-use crate::networks::webrtc::{DataChannel, IceCandidate, PeerConnection};
+use wasm_bindgen::prelude::*;
+use web_sys::{MessageEvent, RtcPeerConnectionIceEvent};
+use std::{rc::Rc, cell::RefCell, collections::BTreeMap};
+use crate::networks::webrtc::{Configuration, ConfigurationBuilder, DataChannel, PeerConnection};
+pub use crate::networks::webrtc::IceCandidate;
 
-struct PeerData {
-    connection: PeerConnection,
-    channel: DataChannel,
-    #[allow(clippy::type_complexity)]
-    closures: (
-        Closure<dyn FnMut()>,
-        Closure<dyn FnMut()>,
-        Closure<dyn FnMut()>,
-    ),
+pub struct PeerHandshakeData {
+    source_id: u32,
+    target_id: u32,
+    sdp_description: String,
+    ice_candidates: Vec<IceCandidate>,
 }
 enum PeerStatus {
-    Disconnected,
-    Connecting(PeerData, Vec<IceCandidate>),
-    Connected(PeerData),
+    Connecting(PeerHandshakeData),
+    Connected,
 }
-struct PeerNetwork {
+struct PeerData {
+    status: PeerStatus,
+    connection: PeerConnection,
+    channel: DataChannel,
+    connection_state_callback: Closure<dyn FnMut()>,
+    ice_candidate_callback: Closure<dyn FnMut(RtcPeerConnectionIceEvent)>,
+    open_callback: Closure<dyn FnMut()>,
+    message_callback: Closure<dyn FnMut(MessageEvent)>,
+}
+struct PeerNetworkData {
+    id: u32,
+    peers: BTreeMap<u32, PeerData>,
+    configuration: Configuration,
+}
+pub struct PeerNetwork {
+    network_data: Rc<RefCell<PeerNetworkData>>,
+}
+impl PeerNetwork {
+    pub fn new() -> Self {
+        let configuration = ConfigurationBuilder::new()
+            .add_turn_server("turn:turn.joongle.dev:3478", "guest", "guest1234")
+            .add_turn_server("turn:turn.joongle.dev:5349", "guest", "guest1234")
+            .build();
+        Self {
+            network_data: Rc::new(RefCell::new(PeerNetworkData {
+                id: 0,
+                peers: BTreeMap::new(),
+                configuration,
+            }))
+        }
+    }
+    pub fn set_id(&self, id: u32) {
+        self.network_data.borrow_mut().id = id;
+    }
+    pub async fn create_handshake_data<F: FnMut(PeerHandshakeData)>(&self, peer_id: u32, ) -> PeerHandshakeData {
 
+        todo!()
+    }
 }
