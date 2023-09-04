9decaa97c30ec83d9797702595f149e994256bf9
Joongho <leejoongho42@gmail.com>
2023-09-04 17:11:46 -0400

    change background color to casino green

--- crates-wasm32/yahtzee/src/graphics/mod.rs
+++ crates-wasm32/yahtzee/src/graphics/mod.rs
@@ -96,10 +96,10 @@ impl Renderer {
                     resolve_target: None,
                     ops: wgpu::Operations {
                         load: wgpu::LoadOp::Clear(wgpu::Color {
-                            r: 0.1,
-                            g: 0.2,
-                            b: 0.3,
-                            a: 1.0,
+                            r: 0.180392,
+                            g: 0.301960,
+                            b: 0.243137,
+                            a: 1.000000,
                         }),
                         store: true,
                     },
