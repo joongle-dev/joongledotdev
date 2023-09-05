0469b3628d3b4b5c44b51d2a8239109b4efa0b28
Joongho <leejoongho42@gmail.com>
2023-09-05 16:34:04 -0400

    run wasm-opt and gzip

--- crates-wasm32/build.sh
+++ crates-wasm32/build.sh
@@ -2,3 +2,6 @@
 export RUSTFLAGS=--cfg=web_sys_unstable_apis
 cargo build --target=wasm32-unknown-unknown --release
 wasm-bindgen --web --no-typescript --out-dir=../assets/yahtzee target/wasm32-unknown-unknown/release/yahtzee.wasm
+wasm-opt -o ../assets/yahtzee/yahtzee_bg.wasm -Os ../assets/yahtzee/yahtzee_bg.wasm
+gzip -k ../assets/yahtzee/yahtzee_bg.wasm
+read pause
