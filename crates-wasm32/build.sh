1cb961132424544b3c3c00d0a9169febe5878fec
Joongho <leejoongho42@gmail.com>
2023-09-06 01:33:29 -0400

    gzip force rewrite

--- crates-wasm32/build.sh
+++ crates-wasm32/build.sh
@@ -3,5 +3,4 @@ export RUSTFLAGS=--cfg=web_sys_unstable_apis
 cargo build --target=wasm32-unknown-unknown --release
 wasm-bindgen --web --no-typescript --out-dir=../assets/yahtzee target/wasm32-unknown-unknown/release/yahtzee.wasm
 wasm-opt -o ../assets/yahtzee/yahtzee_bg.wasm -Os ../assets/yahtzee/yahtzee_bg.wasm
-gzip -k ../assets/yahtzee/yahtzee_bg.wasm
-read pause
+gzip -kf ../assets/yahtzee/yahtzee_bg.wasm
