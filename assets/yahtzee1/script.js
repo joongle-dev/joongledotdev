5b10a03147dd76063bd8873edda11a85df57685f
Joongho <leejoongho42@gmail.com>
2025-03-06 06:59:21 -0500

    websocket behavior test

--- assets/yahtzee1/script.js
+++ assets/yahtzee1/script.js
@@ -2,11 +2,27 @@ const join_button = document.getElementById('join-button');
 const create_button = document.getElementById('create-button');
 const search_params = new URLSearchParams(window.location.search);
 const room_id = search_params.get('room');
-let socket;
+const socket = new WebSocket('wss://joongle.dev/yahtzee1/ws');
+socket.binaryType = 'arraybuffer';
+socket.onopen = () => {
+    console.log('open event');
+}
+socket.onclose = (event) => {
+    switch (event.reason) {
+        case 'disconnect':
+            break;
+        case 'not found':
+            break;
+        case 'full':
+            break;
+        default:
+    }
+    console.log("close event")
+}
 if (room_id) {
     join_button.disabled = false;
     join_button.addEventListener('click', () => {
-        socket = new WebSocket('wss://joongle.dev/yahtzee1/ws?' + room_id);
+        //socket = new WebSocket('wss://joongle.dev/yahtzee1/ws?' + room_id);
         socket.binaryType = 'arraybuffer';
         socket.onopen = () => {
             console.log('open event');
@@ -26,7 +42,7 @@ if (room_id) {
     });
 }
 create_button.addEventListener('click', () => {
-    socket = new WebSocket('wss://joongle.dev/yahtzee1/ws');
+    //socket = new WebSocket('wss://joongle.dev/yahtzee1/ws');
     socket.binaryType = 'arraybuffer';
     socket.onopen = () => {
         console.log('open event');
