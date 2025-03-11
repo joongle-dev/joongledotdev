e3d0b5339ca1cd1d3fa2299345cb96bef4ab391e
Joongho <leejoongho42@gmail.com>
2025-03-11 04:45:47 -0400

    json test

--- assets/yahtzee1/script.js
+++ assets/yahtzee1/script.js
@@ -2,39 +2,26 @@ const join_button = document.getElementById('join-button');
 const create_button = document.getElementById('create-button');
 const search_params = new URLSearchParams(window.location.search);
 const room_id = search_params.get('room');
-let socket;
+const socket = new WebSocket('wss://joongle.dev/yahtzee1/ws');
+socket.binaryType = 'arraybuffer';
+socket.onopen = () => {
+    socket.onmessage = (event) => {
+        console.log('message event: ' + event.data)
+    }
+    console.log('open event');
+}
+socket.onclose = (event) => {
+    console.log('close event: ', event.code, ': ',event.reason);
+}
+socket.onerror = (event) => {
+    console.log('error event: ' + event);
+}
 if (room_id) {
     join_button.disabled = false;
     join_button.addEventListener('click', () => {
-        socket = new WebSocket('wss://joongle.dev/yahtzee1/ws?room=' + room_id);
-        socket.binaryType = 'arraybuffer';
-        socket.onopen = () => {
-            socket.onmessage = (event) => {
-                console.log('message event: ' + event.data)
-            }
-            console.log('open event');
-        }
-        socket.onclose = (event) => {
-            console.log('close event: ', event.code, ': ',event.reason);
-        }
-        socket.onerror = (event) => {
-            console.log('error event: ' + event);
-        }
+        socket.send('{"type":"ClientEvent","room":"abc","name":"def"}');
     });
 }
 create_button.addEventListener('click', () => {
-    socket = new WebSocket('wss://joongle.dev/yahtzee1/ws');
-    socket.binaryType = 'arraybuffer';
-    socket.onopen = () => {
-        socket.onmessage = (event) => {
-            console.log('message event: ' + event.data)
-        }
-        console.log('open event');
-    }
-    socket.onclose = (event) => {
-        console.log('close event: ', event.code, ': ',event.reason);
-    }
-    socket.onerror = (event) => {
-        console.log('error event: ' + event);
-    }
+    socket.send('{"type":"ClientEvent","name":"def"}');
 });
--- crates/server/src/yahtzee1/mod.rs
+++ crates/server/src/yahtzee1/mod.rs
@@ -1,35 +1,56 @@
 use std::net::SocketAddr;
-use axum::{extract::{ConnectInfo, WebSocketUpgrade}, response::IntoResponse, routing::get, Router};
-use axum::extract::Query;
-use axum::extract::ws::Message;
-use axum::http::StatusCode;
+use std::sync::{Arc, Mutex};
+use axum::{extract::{ConnectInfo, WebSocketUpgrade}, routing::get, Router};
+use axum::extract::ws::{Message, WebSocket};
 use axum::response::Response;
-use serde::Deserialize;
+use futures::stream::SplitSink;
+use futures::sink::SinkExt;
+use futures::StreamExt;
+use serde::{Deserialize, Serialize};
 
 pub fn routes() -> Router {
     Router::new()
         .route("/ws", get(lobby_connection_handler))
 }
 
-#[derive(Deserialize)]
-struct RoomQuery {
-    room: Option<String>,
-}
 async fn lobby_connection_handler(
     websocket_upgrade: WebSocketUpgrade,
-    Query(query): Query<RoomQuery>,
     ConnectInfo(addr): ConnectInfo<SocketAddr>,
 ) -> Response {
-    println!("->> New connection at {addr}");
-    if query.room.is_some() {
-        (StatusCode::INTERNAL_SERVER_ERROR, "Request cannot be fulfilled").into_response()
-    }
-    else {
-        websocket_upgrade.on_upgrade(move |mut websocket| async move {
-            match websocket.send(Message::Text("pong".into())).await {
-                Ok(_) => println!("->> Successfully ponged {addr}"),
-                Err(_) => println!("->> Error ponging {addr}")
+    websocket_upgrade.on_upgrade(move |mut websocket| async move {
+        let (mut sender, mut receiver) = websocket.split();
+        while let Some(Ok(Message::Text(message))) = receiver.next().await {
+            if let Ok(message) = serde_json::from_str::<ClientEvent>(&message) {
+                let _ = sender.send(Message::text("received client event")).await;
             }
-        })
+            else {
+                let _ = sender.send(Message::text("invalid client event")).await;
+            }
+        }
+    })
+}
+
+struct Player(SplitSink<WebSocket, Message>);
+struct PlayerList([Option<Player>; 4]);
+struct Room(Arc<Mutex<PlayerList>>);
+#[derive(Deserialize, Clone)]
+#[serde(tag = "type")]
+enum ClientEvent {
+    Join {
+        room: Option<String>,
+        name: String,
+    }
+}
+#[derive(Serialize, Clone)]
+#[serde(tag = "type")]
+enum ServerEvent {
+    JoinSuccess {
+        id: usize,
+    },
+    JoinFail {
+        reason: String,
+    },
+    Join {
+        name: String,
     }
 }
