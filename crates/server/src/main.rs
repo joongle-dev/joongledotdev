dfe1ef940b66fe8b40acd2f468882e0ff8816dc4
Joongho <leejoongho42@gmail.com>
2025-09-08 06:27:36 -0400

    serve certbot challenge over http

--- crates/server/src/main.rs
+++ crates/server/src/main.rs
@@ -2,7 +2,7 @@ pub mod error;
 pub mod yahtzee;
 mod yahtzee1;
 
-use axum::{Router, response::IntoResponse, handler::HandlerWithoutStateExt};
+use axum::Router;
 use axum_server::tls_rustls::RustlsConfig;
 use std::net::SocketAddr;
 use tower_http::services::{ServeDir, ServeFile};
@@ -17,26 +17,23 @@ const KEY_FILE: &str = "certs/key.pem";
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let routes = Router::new()
-        .fallback_service(ServeDir::new("assets")
-            .precompressed_gzip()
-            .not_found_service(ServeFile::new("assets/not_found.html")))
+    let https_routes = Router::new()
+        .fallback_service(ServeDir::new("assets").precompressed_gzip().not_found_service(ServeFile::new("assets/not_found.html")))
         .nest("/yahtzee", yahtzee::routes())
         .nest("/yahtzee1", yahtzee1::routes());
-
+    let http_routes = Router::new()
+        .nest_service("/.well-known/acme-challenge/", ServeDir::new("/.well-known/acme-challenge/"));
     match RustlsConfig::from_pem_file(CERT_FILE, KEY_FILE).await {
         Ok(config) => {
             println!("->> Found certificates!, Running in encrypted mode.");
-            // tokio::spawn(redirect_http_to_https());
-            let addr = SocketAddr::from((IP_ADDR, HTTPS_PORT));
-            println!("->> Listening on {addr}.");
-            let _ = axum_server::bind_rustls(addr, config).serve(routes.into_make_service_with_connect_info::<SocketAddr>()).await;
+            let https_addr = SocketAddr::from((IP_ADDR, HTTPS_PORT));
+            let https = tokio::task::spawn(axum_server::bind_rustls(https_addr, config).serve(https_routes.into_make_service_with_connect_info::<SocketAddr>()));
+            let http_addr = SocketAddr::from((IP_ADDR, HTTP_PORT));
+            let http = tokio::task::spawn(axum_server::bind(http_addr).serve(http_routes.into_make_service_with_connect_info::<SocketAddr>()));
+            let _ = tokio::join!(https, http);
         }
         Err(error) => {
-            println!("->> Failed to validate certificates: {error}. Running in unencrypted mode.");
-            let addr = SocketAddr::from((IP_ADDR, HTTP_PORT));
-            println!("->> Listening on {addr}.");
-            let _ = axum_server::bind(addr).serve(routes.into_make_service_with_connect_info::<SocketAddr>()).await;
+            println!("->> Failed to validate certificates: {error}.");
         }
     }
 
